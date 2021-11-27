80a694a07a9dc09cd5f7a78cfad45f4810311800
farahh001 <fsultan001@citymail.cuny.edu>
2021-11-26 23:38:03 -0500

    all pages fixed and it's working fine

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     role = models.CharField(choices=ROLES, max_length=50)
     image = models.ImageField(upload_to='profile_pics/')
-
+    
     def get_image(self):
         try:
             return self.image.url
@@ -25,3 +25,13 @@ def update_profile_signal(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+class Application(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    role = models.CharField(choices=ROLES, max_length=50)
+    document = models.FileField(upload_to='applications/', blank=False)
+    approved = models.BooleanField(default=False)
+    rejected = models.BooleanField(default=False)
+
+

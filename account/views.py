4d9a27c6e60fc98b23702531f6c89c53286fa865
sanjidanisha <snisha000@citymail.cuny.edu>
2021-11-26 19:00:12 -0500

    contactus views.py and added admin.html

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls.base import reverse
 
 # Create your views here.
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'course/contactus.html')

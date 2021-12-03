edfaa934994c25f8d3a98df5e541a63d074ef0be
farahh001 <fsultan001@citymail.cuny.edu>
2021-12-03 11:51:34 -0500

    user-reviews added on Profile

--- course/models.py
+++ course/models.py
@@ -1,15 +1,57 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 # Create your models here.
+
+
+
+
+
+class Class(models.Model):
+
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='thumbnails/', null=True, blank=True, default="default.jpg")
+    instructions = tinymce_models.HTMLField(null=True, blank=True)
+    quota = models.PositiveIntegerField(default=0)
+    instructor = models.ForeignKey("accounts.Profile", on_delete=models.CASCADE, related_name="assigned_classes", null=True, blank=True)
+    enrolled_by = models.ManyToManyField("accounts.Profile", related_name="enrolled_classes", null=True, blank=True)
+    completed_by = models.ManyToManyField("accounts.Profile", related_name="completed_classes", null=True, blank=True)
+    start_date = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    start_time = models.TimeField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    end_time = models.TimeField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    cancelled = models.BooleanField(default=False)
+    rating = models.FloatField(default=0)
+
+    @property
+    def get_thumbnail(self):
+        try:
+            return self.thumbnail.url
+        except:
+            return ''
+
+
+
 
 
 class Course(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnails/')
-    
+
     def get_thumbnail(self):
         try:
             return self.thumbnail.url
         except:
             return ''
+
+    @property
+    def get_enroll_requests(self):
+        requests = []
+        classes = Class.objects.filter(instructor__id = self.id)
+        print(classes)
+        for _class in classes:
+            for request in _class.enroll_requests.all():
+                requests.append(request)
+        return requests

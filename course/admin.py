6c90a0ee200efe671551a1d1c05aec1225ed24ed
farahh001 <fsultan001@citymail.cuny.edu>
2021-12-07 00:31:56 -0500

    statics added and shopping cart updated

--- course/admin.py
+++ course/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-
+from .models import Class, Semester, Warning, Complain, TabooWord
+from accounts.models import Grade
 
 # Register your models here.
+
+admin.site.register(Class)
+admin.site.register(Semester)
+admin.site.register(Grade)
+admin.site.register(Warning)
+admin.site.register(Complain)
+admin.site.register(TabooWord)
